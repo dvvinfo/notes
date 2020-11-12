cecdc4680b0805b9684c3076142117da21b00c59
Vladimir Drobyshev <dvvinfo@mail.ru>
2020-11-12 17:55:42 +0300

    initialization of the project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="wrapper">
+  <!-- header -->
+  <div class="wrapper-content">
+    <section>
+      <div class="container">
+        <h1>{{ title }}</h1>
+        <div class="message" v-if="message">
+            <p>{{ message }}</p>
+        </div>
+        <!-- new note -->
+        <div class="new-note">
+            <input type="text" v-model="note.title">
+            <textarea v-model="note.descr"></textarea>
+            <button @click="addNote">Добавить заметку</button>
+        </div>
+        <!-- note-list -->
+        <div class="notes">
+            <div class="note" v-for="(note, index) in notes" :key="index">
+                <div class="note__header"><p>{{ note.title }}</p></div>
+                <div class="note__body"><p>{{ note.descr }}</p>
+                    <span>{{ note.date }}</span>
+                
+                </div>
+            </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+
+  <!-- footer -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      title: 'Приложение "Заметки"',
+                message: null,
+                note: {
+                    title: '',
+                    descr: ''
+                },
+                notes: [
+                    {
+                        title: 'Первая заметка',
+                        descr: 'Описание первой заметки',
+                        date: new Date(Date.now()).toLocaleString()
+                    },
+                    {
+                        title: 'Вторая заметка',
+                        descr: 'Описание второй заметки',
+                        date: new Date(Date.now()).toLocaleString()
+                    },
+                    {
+                        title: 'Третья заметка',
+                        descr: 'Описание третьей заметки',
+                        date: new Date(Date.now()).toLocaleString()
+                    },
+                ]
+
+    }
+
+  },
+   methods: {
+                addNote() {
+                    // let {title, descr} = this.note
+                    if(this.note.title === '') {
+                        this.message = 'Заголовок не может быть пустым!'
+                        return false
+                    }
+                    if(this.note.descr === '') {
+                        this.message = 'Описание не может быть пустым!'
+                        return false
+                    }
+                    this.notes.push({
+                        title: this.note.title,
+                        descr: this.note.descr,
+                        date:  new Date(Date.now()).toLocaleString()
+                    })
+                    this.note.title = ''
+                    this.note.descr = ''
+                    this.message = null
+                }
+            }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    pubLicPath: process.env.NODE_ENV === 'production'
+    ? '/'
+    : '/',
+    assetsDir: '',
+    productionSourceMap: false
+}
