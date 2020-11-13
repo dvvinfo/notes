2e4f801108d1acf3bbd159c3761ae071555a8203
Vladimir Drobyshev <dvvinfo@mail.ru>
2020-11-13 14:49:30 +0300

    Added animation and switch icons

--- src/App.vue
+++ src/App.vue
@@ -4,18 +4,28 @@
   <div class="wrapper-content">
     <section>
       <div class="container">
-          <!-- title -->
-        <h1>{{ title }}</h1>
+      
         <!-- message -->
         <message v-if="message" :message="message"/>
         <!-- new note -->
         <newNote
             :note="note"
             @addNote="addNote"/>
+                <!-- title -->
+        <div class="note__header">
+            <h1>{{ title }}</h1>
+            <div class="icons">
+                <svg :class="{ active: grid }" @click="grid = true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
+                <svg :class="{ active: !grid }" @click="grid = false" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
+
+            </div>
+        </div>        
+        
             <!-- note list -->
         <notes
         :notes="notes"
         @remove="removeNote"
+        :grid="grid"
         />
       </div>
     </section>
@@ -41,6 +51,7 @@ export default {
     return {
       title: 'Приложение "Заметки"',
                 message: null,
+                grid: true,
                 note: {
                     title: '',
                     descr: ''
--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,8 +1,8 @@
 <template>
     <!-- note-list -->
         <div class="notes">
-            <div class="note" v-for="(note, index) in notes" :key="index">
-                <div class="note__header"><p>{{ note.title }}</p>
+            <div class="note" :class="{ full: !grid}" v-for="(note, index) in notes" :key="index">
+                <div class="note__header" :class="{ full: !grid}"><p>{{ note.title }}</p>
                 <p class="note_del" @click="removeNote(index)">X</p>
                 </div>
                 <div class="note__body"><p>{{ note.descr }}</p>
@@ -19,7 +19,11 @@ export default {
         notes: {
             type: Array,
             required: true
-        }
+        },
+        grid: {
+            type: Boolean,
+            required: true
+        },
 
     },
     methods: {
@@ -43,15 +47,49 @@ export default {
     padding: 18px 20px;
     margin-bottom: 20px;
     background-color: #fff;
+    transition: all .25s cubic-bezier(.02,.01,.47,1);
+    box-shadow: 0 30px 30px rgba(0,0,0,.02);
+    &:hover {
+    box-shadow: 0 30px 30px rgba(0,0,0,.04);
+    transform: translate(0,-6px);
+    transition-delay: 0s !important;
+    }
+    &.full {
+        width: 100%;
+    }
 }
-.note__header{
+.note__header {
     display: flex;
     align-items: center;
     justify-content: space-between;
-    p{
+    h1 {
+        font-size: 32px;
+    }
+    p {
         color: #402caf;
         font-size: 22px;
     }
+    svg {
+        margin-right: 12px;
+        color: #999999;
+        cursor: pointer;
+        &.active {
+            color: #402caf;
+        }
+        &:last-child {
+            margin-right: 0;
+        }
+
+    }
+    &.full {
+    justify-content: center;
+    p {
+      margin-right: 20px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+  }
 }
 .note__body {
     p {
