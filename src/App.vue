a36000bb8b65d37ea2c29dff075706a3042a4137
Vladimir Drobyshev <dvvinfo@mail.ru>
2021-01-12 14:31:20 +0300

    Added note search

--- src/App.vue
+++ src/App.vue
@@ -4,26 +4,25 @@
   <div class="wrapper-content">
     <section>
       <div class="container">
-      
         <!-- message -->
         <message v-if="message" :message="message"/>
         <!-- new note -->
         <newNote
             :note="note"
             @addNote="addNote"/>
-                <!-- title -->
-        <div class="note__header">
+        <div class="note__header" style="margin: 36px 0">
+             <!-- title -->
             <h1>{{ title }}</h1>
+            <search :value="search" placeholder="Поиск заметок" @search ="search = $event"/>
             <div class="icons">
                 <svg :class="{ active: grid }" @click="grid = true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" ><rect x="3" y="3" width="7" height="7"></rect><rect x="14" y="3" width="7" height="7"></rect><rect x="14" y="14" width="7" height="7"></rect><rect x="3" y="14" width="7" height="7"></rect></svg>
                 <svg :class="{ active: !grid }" @click="grid = false" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="8" y1="6" x2="21" y2="6"></line><line x1="8" y1="12" x2="21" y2="12"></line><line x1="8" y1="18" x2="21" y2="18"></line><line x1="3" y1="6" x2="3" y2="6"></line><line x1="3" y1="12" x2="3" y2="12"></line><line x1="3" y1="18" x2="3" y2="18"></line></svg>
 
             </div>
-        </div>        
-        
+        </div>
             <!-- note list -->
         <notes
-        :notes="notes"
+        :notes="notesFilter"
         @remove="removeNote"
         :grid="grid"
         />
@@ -41,15 +40,17 @@ import message from "@/components/Message.vue";
 import Message from './components/Message.vue';
 import newNote from './components/NewNote.vue';
 import notes from './components/Notes.vue';
-
+import search from './components/Search.vue';
 export default {
   components: {
-        message, newNote, notes
+        message, newNote, notes,search
 
     },
   data () {
     return {
-      title: 'Приложение "Заметки"',
+                title: 'Приложение "Заметки"',
+                search: '',
+
                 message: null,
                 grid: true,
                 note: {
@@ -77,6 +78,23 @@ export default {
     }
 
   },
+  computed: {
+    notesFilter () {
+      let array = this.notes,
+          search = this.search
+      if (!search) return array
+      // Small
+      search = search.trim().toLowerCase()
+      // Filter
+      array = array.filter(function (item) {
+        if (item.title.toLowerCase().indexOf(search) !== -1) {
+          return item
+        }
+      })
+      // Error
+      return array
+    }
+  },
    methods: {
                 addNote() {
                     // let {title, descr} = this.note
@@ -103,7 +121,6 @@ export default {
 
                 }
 
-            
             }
 
 }
